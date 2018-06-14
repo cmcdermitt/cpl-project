a87e39fe493524088f84914f4c07d513acb8af57
cmcdermitt <charliemc1996@gmail.com>
2018-06-14 19:52:25 -0400

    actually added dict of keywords

--- Scanner.py
+++ Scanner.py
@@ -1,2 +1,15 @@
-x = 2
-print(2)
+#List of keywords
+
+keywords = dict(zip(range (101, 155), ['SYMBOL', 'IDENTIFIER', 'HCON', 'FORWARD', 'REFERENCES',
+    'MEXTERN', 'FUNCTION', 'MAIN', 'RETURN', 'POINTER', 'ARRAY', 'LB', 'RB','ICON',
+    'TYPE', 'STRUCT', 'STRUCTYPE', 'MVOID', 'INTEGER',
+    'SHORT', 'REAL', 'FLOAT', 'DOUBLE', 'TBOOL',
+    'CHAR', 'TSTRING', 'OF', 'LENGTH', 'ICON',
+    'TBYTE', 'SPECIFICATIONS', 'ENUM', 'STRUCT', 'GLOBAL',
+    'DECLARATIONS', 'IMPLEMENTATIONS', 'FUNCTION', 'MAIN', 'PARAMETERS',
+    'COMMA', 'CONSTANT', 'BEGIN', 'ENDFUN', 'IF',
+    'THEN', 'ELSE', 'ENDIF', 'WHILE', 'ENDWHILE',
+    'LET', 'REPEAT', 'UNTIL', 'ENDREPEAT', 'DISPLAY']))
+
+
+print (keywords) 
