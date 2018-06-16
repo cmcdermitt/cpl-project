ad439522aeea2c48f9c2e3a68c5244a081841d95
cmcdermitt <charliemc1996@gmail.com>
2018-06-16 00:04:03 -0400

    Added file IO, started basic structure

---
+++ scanner.py
@@ -0,0 +1,41 @@
+#imports
+import sys
+import csv
+
+#List of keywords
+
+keywords = dict(zip(['SYMBOL', 'IDENTIFIER', 'HCON', 'FORWARD', 'REFERENCES',
+    'MEXTERN', 'FUNCTION', 'MAIN', 'RETURN', 'POINTER', 'ARRAY', 'LB', 'RB','ICON',
+    'TYPE', 'STRUCT', 'STRUCTYPE', 'MVOID', 'INTEGER',
+    'SHORT', 'REAL', 'FLOAT', 'DOUBLE', 'TBOOL',
+    'CHAR', 'TSTRING', 'OF', 'LENGTH', 'ICON',
+    'TBYTE', 'SPECIFICATIONS', 'ENUM', 'STRUCT', 'GLOBAL',
+    'DECLARATIONS', 'IMPLEMENTATIONS', 'FUNCTION', 'MAIN', 'PARAMETERS',
+    'COMMA', 'CONSTANT', 'BEGIN', 'ENDFUN', 'IF',
+    'THEN', 'ELSE', 'ENDIF', 'WHILE', 'ENDWHILE',
+    'LET', 'REPEAT', 'UNTIL', 'ENDREPEAT', 'DISPLAY'], range(101, 155)))
+
+tokens = []
+
+def main(argv):
+    print(sys.argv[1])
+    linenum = 0
+    with open(sys.argv[1]) as infile: #open the file, sys.argv[1] is the first command line argument
+        for textline in infile:
+            linenum += 1
+            line = textline.split() #splits the line into a list of words wherever there's whitespace
+            #PROBLEM: Not all tokens will be separated by whitespace. We'll have to do this better later.
+
+            for word in line:
+                tokens.append([word, linenum])
+
+    print tokens
+
+    with open('output_scanned.csv', 'w') as outfile: #open output file
+        writer = csv.writer(outfile)
+        writer.writerows(tokens) #writes each item in tokens to a row of the .csv
+
+	
+print (keywords) 
+if __name__ == "__main__":
+	main(sys.argv[1])
