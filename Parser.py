77fb867557245dd9862139b4fefa40b4031253ae
Thinker222 <es0197@yahoo.com>
2018-07-04 12:20:34 -0400

    func_main almost complete
    - All the components of func_main
    are operational
    - Error messages are included properly
    - Rewind token function added to scanner.py for recursive definitions
    - More testing is needed :D

--- Parser.py
+++ Parser.py
@@ -4,26 +4,26 @@ import sys
 
 '''
 Parser.py
-The parser uses the recursive-descent method of parsing, 
+The parser uses the recursive-descent method of parsing,
 where each left hand definition is represented as a function.
-The root of the parse tree is the function program. 
+The root of the parse tree is the function program.
 Each function calls the functions of the right hand components after checking if it is possible to use those components by looking ahead
-The original caller has a list called lexeme_list. lexeme_list is appended the return value of each function called. Each return value is a list. 
+The original caller has a list called lexeme_list. lexeme_list is appended the return value of each function called. Each return value is a list.
 Each of those functions in turn calls other subfunctions that will append the return value of the called function to their own lexeme_list.
-However, there are two cases to consider. 
-If there are multiple right hand definitions, only one will be correct. Sometimes, the wrong function will be chosen first and as a result nothing will be returned. 
-If this is the case, then the integer current_lex needs to be reverted to its value before calling the function. 
+However, there are two cases to consider.
+If there are multiple right hand definitions, only one will be correct. Sometimes, the wrong function will be chosen first and as a result nothing will be returned.
+If this is the case, then the integer current_lex needs to be reverted to its value before calling the function.
 The document has errors in it. If all right hand definitions are exhausted and none of them work (at a certain level), then an error message is created at some level
-In this case, the c_lex value must not be reverted, because parsing must recover and continue. 
-ISSUE: Where do we decide to put the error. Theoretically, it could be at the root of the tree. 
+In this case, the c_lex value must not be reverted, because parsing must recover and continue.
+ISSUE: Where do we decide to put the error. Theoretically, it could be at the root of the tree.
 POSSIBLE SOLUTION: Certain definitions are considered "choke points for errors" like action_def. RESOLVED
-If one action_def in a pactions is malformed, the pactions should continue to process more action_defs.   
+If one action_def in a pactions is malformed, the pactions should continue to process more action_defs.
 lexeme_list = a recursive list lexemes [lexeme[[sublexeme1[sub sub lexeme]] ... [sublexeme[sublexeme]]]]
 ISSUE: When do we decide to add more lexemes to the initial pool of lexemes.
 POSSIBLE SOLUTION: We call the scanner and get all of the lexemes at once. We then append Nothing to the end to signify the last lexeme has been read. RESOLVED: Addded as needed with look back functions
 '''
 
-''' 
+'''
 Sorry for globals, but every function needs these, (unless we pass object?):
 	global c_lex
 	global t_lex
@@ -48,22 +48,22 @@ def returnTabs(tabNum):
 		tabs = tabs + '\t'
 	return tabs
 
-# Returns the current lex	
+# Returns the current lex
 def nextLex():
 	return ['1','2','KEYWORD','MAIN'] # Will be implemented later with scanner
 
-#Will return the current lex 
+#Will return the current lex
 def currentLex():
-	return ['3','4','KEYWORD','RETURN'] 
+	return ['3','4','KEYWORD','RETURN']
 
 def prevLex():
-		return [] #Will return the last lex 
-	
-	
+		return [] #Will return the last lex
+
+
 # Prints out the tree using tabs to represent children
-# 
+#
 def printTree(tree_list, tab):
-	if(len(tree_list) == 0): 
+	if(len(tree_list) == 0):
 		return
 	print(returnTabs(tab) + tree_list[0]) # Print out the first item in the list; this is the parent node
 	if(len(tree_list) == 1):
@@ -76,7 +76,7 @@ def printTree(tree_list, tab):
 		else:
 			print(returnTabs(tab + 1) + str(tree_list[x]))
 
-# Starting point for parse tree 
+# Starting point for parse tree
 # Initially, lex_list is ['Program']
 # After appending func_main(), lex_list might be ['Program',[func_main, ['0','0','KEYWORD','FUNCTION'], ['1','1',IDENTIFER, 'MAIN'], [oper_type,['2','2','KEYWORD','RETURN']]]]
 # The second pair of braces represents the entirety of func_main
@@ -85,8 +85,7 @@ def program_start():
 	lex_list = ['Program']
 	lex_list.append(func_main())
 	printTree(lex_list, 0)
-	print(scanner.getCurrentToken())
-	
+
 def func_main():
 	lex_list = ['func_main']
 	lex = scanner.getNextToken()
@@ -96,7 +95,6 @@ def func_main():
 	elif(lex[lex_en['value']] == 'FUNCTION'):
 		lex_list.append(tuple(lex))
 		lex = scanner.getNextToken()
-		print(lex)
 		if(lex[lex_en['type']] == 'IDENTIFIER'):
 			lex_list.append(tuple(lex))
 			lex = scanner.getNextToken()
@@ -112,10 +110,10 @@ def func_main():
 
 def globals():
 	lex_list = ['globals']
-	
-	
+
+
 def oper_type():
-	lex_list = ['oper_type']	
+	lex_list = ['oper_type']
 	lex_list.append(tuple(scanner.getCurrentToken()))
 	lex = scanner.getNextToken()
 	if(lex[lex_en['value']] == 'POINTER'):
@@ -125,12 +123,14 @@ def oper_type():
 		lex_list.append(chk_array())
 		lex = scanner.getNextToken()
 	word = lex[lex_en['value']]
-	if(word == 'TYPE' or 'STRUCT' or 'IDENTIFIER'):
+	if(word == 'TYPE' or word == 'STRUCT' or word == 'IDENTIFIER'):
 		lex_list.append(ret_type())
+	else:
+		lex_list.append('\tError: The keywords STRUCT or TYPE or an IDENTIFIER was expected')
 	# Not done yet
 	return lex_list
-	
-	
+
+
 def chk_ptr():
 	lex_list = ['chk_ptr']
 	lex_list.append(tuple(scanner.getCurrentToken()))
@@ -145,9 +145,13 @@ def chk_array():
 	lex_list = ['chk_array']
 	lex_list.append(tuple(scanner.getCurrentToken()))
 	lex = scanner.getNextToken()
+	if(lex[lex_en['value']] != 'LB'):
+		lex_list.append('\t Error Keyword LB expected')
+		return lex_list
 	while(lex[lex_en['value']] == 'LB'):
 		lex_list.append(array_index())
 		lex = scanner.getNextToken()
+	scanner.rewindCurrentToken()
 	return lex_list
 
 def array_index():
@@ -163,41 +167,51 @@ def array_index():
 	if(lex[lex_en['value']] == 'RB'):
 		lex_list.append(tuple(lex))
 	else:
-		lex_list.append('\tError: KEYWORD RB was expected')
+		lex_list.append('\tError: Keyword RB was expected')
 	return lex_list
-	
+
 def ret_type():
-	lex_list = []
+	lex_list = ['ret_type']
 	lex = scanner.getCurrentToken()
 	word = lex[lex_en['value']]
 	lex_list.append(tuple(lex))
-	lex = scanner.getNextToken()
 	if(word == 'TYPE'):
 		type = lex[lex_en['type']]
 		if(type == 'MVOID' or type == 'INTEGER' or type == 'REAL' or type == 'TBOOL' or type == 'CHAR' or type == 'TSTRING'):
 			lex_list.append(type_name())
 			return lex_list
+	if(lex[lex_en['value']] == 'STRUCT' or lex[lex_en['value']] == 'STRUCTYPE'): #STRUCTTYPE?
+		lex = scanner.getNextToken()
+		if(lex[lex_en['type']] == 'IDENTIFIER'):
+			lex_list.append(tuple(lex))
+			return lex_list
+		else:
+			lex_list.append('\tError: Identifier was expected')
+			return lex_list
+
+	return lex_list
+
+def type_name():
+	lex_list = []
+	lex = scanner.getCurrentToken()
+	lex_list.append(tuple(lex))
+	return lex_list
+'''	if(lex[lex_en['value']] == 'TSTRING'):
+		lex_list.append(lex)
+		lex = scanner.getNextToken()
+		if(lex[lex_en['value']] == 'OF'):  Is TSTRING relevant? ICON is an
+		unknown keyword'''
 
 
-		
-	
+
+
 def array_dim_list():
 	global c_lex
 	global t_lex
 	temp_c_lex
 	lexeme_list = []
-	
-def arrayIndex():
-	global c_lex
-	global t_lex
-	temp_c_lex
-	lexeme_list = []
-	
-def type_name():
-	global c_lex
-	global t_lex
-	temp_c_lex
-	lexeme_list = []
+
+
 
 if __name__ == '__main__':
 	program_start()
