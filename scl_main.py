e47ac6dae9a5c3c899b7cb445f3af239c28cf60a
cmcdermitt <charliemc1996@gmail.com>
2018-07-11 20:59:58 -0400

    fixed file output

--- scl_main.py
+++ scl_main.py
@@ -11,26 +11,27 @@ import sys
 
 def main():
 	lex_tree = scl_parser.parse()
-	print(printAnnotatedTree(lex_tree, 0, True))
+	if len(sys.argv) > 2:
+		with open(sys.argv[2], 'w') as outfile:
+			outfile.write(printAnnotatedTree(lex_tree, 0, True))
+	else:
+		print(printAnnotatedTree(lex_tree, 0, True))
 
 
 # Prints out the tree using tabs to represent children
 def printTree(tree_list, tab, out_string = ''):
 	if(len(tree_list) == 0):
 		return
-	if len(sys.argv) > 2: #if there's an output file name
-		out_string = out_string + returnTabs(tab) + tree_list[0] + '\n' # Print out the first item in the list; this is the parent node
-		if(len(tree_list) == 1):
-			return
-		for x in range(1, len(tree_list)): # Print out all of its children
-			if(isinstance(tree_list[x], str)): # If the child is a string, print it out
-				out_string = out_string + returnTabs(tab) + tree_list[x] + '\n'
-			elif(isinstance(tree_list[x], list)): #If the child is a list, indent by 1 and print out that list
-				out_string = printTree(tree_list[x], tab + 1, out_string)
-			else:
-				out_string = out_string + returnTabs(tab + 1) + str(tree_list[x]) + '\n'
-		with open(sys.argv[2], 'w') as outfile:
-			outfile.write(out_string)
+	out_string = out_string + returnTabs(tab) + tree_list[0] + '\n' # Print out the first item in the list; this is the parent node
+	if(len(tree_list) == 1):
+		return
+	for x in range(1, len(tree_list)): # Print out all of its children
+		if(isinstance(tree_list[x], str)): # If the child is a string, print it out
+			out_string = out_string + returnTabs(tab) + tree_list[x] + '\n'
+		elif(isinstance(tree_list[x], list)): #If the child is a list, indent by 1 and print out that list
+			out_string = printTree(tree_list[x], tab + 1, out_string)
+		else:
+			out_string = out_string + returnTabs(tab + 1) + str(tree_list[x]) + '\n'
 	return out_string
 
 #Prints a version of the tree with more information
@@ -55,7 +56,7 @@ def printAnnotatedTree(tree_list, tab, printTree = False, out_string = ''):
 def returnTabs(tabNum):
 	tabs = ''
 	for i in range(tabNum):
-		tabs = tabs + '    '
+		tabs = tabs + '  '
 	return tabs
 
 if __name__ == '__main__':
