9aaf5d79198aaf5b84aac42dc85d8b417dd3b21c
cmcdermitt <charliemc1996@gmail.com>
2018-07-16 15:57:54 -0400

    added parser_tree :

---
+++ parser_tree.py
@@ -0,0 +1,42 @@
+terminal_types = ['ICON', 'FCON', 'HCON', 'IDENTIFIER'] #COMPLETE LATER
+
+class Node:
+
+    #empty string for type means nonterminal
+    def __init__ (self, in_val, in_type = '', in_children = []):
+        self.name = in_name
+        self.value = in_val
+        self.type = in_type
+        self.children = in_children
+
+    
+    #returns the child at the given index
+    def getChildAt(self, index):
+        return self.children[index]
+
+    #returns the first child node with type = in_type
+    def getChildOfType(self, in_type):
+        for i, child in enumerate(self.children):
+            if isinstance(child, Node):
+                if child.type = in_type:
+                    return child
+        return false #if the for loop completes without finding a child
+    
+    #returns all child nodes with type = in_type
+    def getChildrenOfType(self, in_type):
+        targets = []
+        for i, child in enumerate(self.children):
+            if child.type = in_type:
+                targets.append(child)
+        return targets
+
+    #adds a child to children
+    def addChild(self, child):
+        self.children.append(child)
+    
+    #returns the index of the first child of that type, or -1 if there isn't one.
+    def hasChild(self, value):
+        for i, child in enumerate(self.children):
+            if child.type = value:
+                return i
+        return false #if the for loop completes without finding a child
