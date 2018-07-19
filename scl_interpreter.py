88b7b107e6213861b8e6834ce5f1bcd73beaabc1
Corey Harris <ckharris96@gmail.com>
2018-07-18 21:07:58 -0400

    Interpreter stuff

--- scl_interpreter.py
+++ scl_interpreter.py
@@ -3,6 +3,10 @@ import scl_var_table
 # Then we execute the main function. <- done in implement
 
 global_vars = scl_var_table.VarTable()
+main_vars = scl_var_table.VarTable()
+controlHeads = []
+breakCalled = False
+
 
 def error(msg, location = ''):
     if location == '':
@@ -41,32 +45,176 @@ def processNode(node):
 # Expected Structure:
 # Type: INPUT
 # Children: IDENTIFIER
-def input(node):
     # Get associated identifier
-    nodeValue = node.children
+    inputValue = node.children[0]
     # Add identifier to variable table
-    variables.declare(nodeValue)
+    main_vars.declare(inputValue)
+    # output results
+    print('Variable ' + inputValue + ' was declared')
     # Node has no children -> IDENTIFIER is just a string
     # Return node to processNode
     return node
 
-def display(node):
-    nodeType = node.type
-    nodeValue = node.children
 # Expected Structure:
-# Type AND
-# Children: pcond1 and pcond 1 or pcond 1 
-def f_and(node):
-    arg1 = processNode(node.children[0])
-    if isinstance(arg1, str):
-        arg1 = lookup(arg1)
-    if len(node.children > 1):
-        arg2 = processNode(node[1])
-        if isinstance(arg2, str):
-            arg2 = lookup(arg2)
-        return arg1 and arg2
+# Type: DISPLAY or DISPLAYN
+# Children: IDENTIFIER
+def f_display(node):
+    # Either DISPLAY or DISPLAYNN
+    nodeType = node.type
+    # Get IDENTIFIER value
+    nodeValue = node.children[0]
+    outputVal = main_vars.getValue(nodeValue)
+    # Output results
+    print(outputVal)
+    # Node has no children -> IDENTIFIER is just a string
+    return node
+
+# Expected Structure:
+# Type: INCREMENT
+# Children: name_ref => IDENTIFIER
+def f_increment(node):
+    # Get node type
+    nodeType = node.type
+    # Get IDENTIFIER variable
+    var = node.children[0]
+    # Get value
+    val = main_vars.getValue(var)
+    # Increment and assign
+    val = val + 1
+    main_vars.assign(var, val)
+    return node
+
+# Expecred Structure:
+# Type: DECREMENT
+# Children: name_ref => IDENTIFIER
+def f_decrement(node):
+    # Get node type
+    nodeType = node.type
+    # Get IDENTIFIER variable
+    var = node.children[0]
+    # Get value
+    val = main_vars.getValue(var)
+    # Decrement and assign
+    val = val - 1
+    main_vars.assign(var.value, val)
+    return node
+
+# Expected Structure:
+# Type: FOR
+# Children: name_ref, expr, ( TO | DOWNTO ), expr, pactions
+def f_for(node):
+    global breakCalled
+    # Get node type
+    nodeType = node.type
+    # Get IDENTIFIER
+    nodeID = processNode(node.children[0])
+    # Get corresponding Python variable
+    var = lookup(nodeID)
+    # Get first expr of for loop
+    expr1 = processNode(node.children[1])
+    # Perform lookup if type string to get value
+    if isinstance(expr1, str):
+        expr1 = lookup(expr1)
+    # Determine if direction is TO or DOWNTO
+    dir = node.children[2].value
+    # Get second expr of for loop
+    expr2 = processNode(node.children[3])
+    # Perform lookup if type string to get value
+    if isinstance(expr2, str):
+        expr2 = lookup(expr2)
+    # Assign initial value to IDENTIFIER
+    main_vars.assign(nodeID, expr1)
+    # Perform for loop up or down
+    if dir == 'TO':
+        while var < expr2:
+            # Process pactions each time
+            p = processNode(node.children[id])
+            if breakCalled == True:
+                breakCalled = False
+                return node
+            # Increment and assign
+            var += 1
+            main_vars.assign(nodeID, var)
     else:
-        return arg1
+        while var > expr2:
+            # Perform pactions each time
+            p = processNode(node.children[id])
+            if breakCalled == True:
+                breakCalled = False
+                return node
+            # Decrement and assign
+            var -= 1
+            main_vars.assign(nodeID, var)
+    return node
+
+# Expected Structure:
+# Type: REPEAT
+# Children pactions, pcondition
+def f_repeat(node):
+    global breakCalled
+    # Get node type
+    nodeType = node.type
+    # Emulate a do-while by executing pactions once,
+    # and repeating in while loop until conditional is false
+    #
+    # Perform initial pactions statement
+    p = processNode(node.children[0])
+    # Get pcondition
+    cond = processNode(node.children[1])
+    # Start while loop
+    while cond:
+        p = processNode(node.children[0])
+        if breakCalled == True:
+            breakCalled = False
+            return node
+        cond = processNode(node.children[1])
+    return node
+
+# Expected Structure:
+# Type: WHILE
+# Children pcondition, pactions
+def f_while(node):
+    global breakCalled
+    # Get node type:
+    nodeType = node.type
+    # Get pcondition
+    cond = processNode(node.children[1])
+    # Start while loop
+    while cond:
+        p = processNode(node.children[0])
+        if breakCalled == True:
+            breakCalled = False
+            return node
+        cond = processNode(node.children[1])
+    return node
+
+# Expected Structure:
+# Type: MBREAK
+# Children: none
+def f_mbreak(node):
+    node = controlHeads.pop()
+    return node
+
+# Expected Structure:
+# Type: MEXIT
+# Children: none
+def f_mexit(node):
+    exit()
+
+# Expected Structure:
+# Type: CALL
+# Children: name_ref => IDENTIFIER
+def f_call(node):
+    # Get node type
+    nodeType = node.type
+    for child in node.children:
+        if isinstance(child, node):
+            processNode(child)
+        else:
+            callValue = main_vars.getValue(node.children[0])
+            print(callValue)
+    return node
+
 
 # Expected Structure:
 # Type OR
