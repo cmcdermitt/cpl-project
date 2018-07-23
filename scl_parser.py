b7392b09ef0fea34337cb25f7877dda1d0ce56de
Thinker222 <es0197@yahoo.com>
2018-07-23 12:33:41 -0400

    name_ref

--- scl_parser.py
+++ scl_parser.py
@@ -853,10 +853,12 @@ def action_def():
 #CASE name_ref
 #GRAMMAR name_ref ::= IDENTIFIER [array_val]
 def name_ref():
+    global current_statement
     node = Node('name_ref')
     # Append function header to output list
     print(scanner.lex[lex_en['value']])
     if scanner.lex[lex_en['type']] == 'IDENTIFIER':
+        current_statement +=  scanner.lex[lex_en['value']] + ' '
         node.children.append(Node(scanner.lex[lex_en['type']], scanner.lex[lex_en['value']]))
         scanner.next()
         if (scanner.lex[lex_en['value']] == 'LB'):
